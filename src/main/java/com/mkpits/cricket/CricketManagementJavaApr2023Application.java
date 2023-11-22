aff55129bf901cacaf5390371e53993a8f5f88bf
Gati Krishna Meher <gatimeher@canoesolutions.com>
2023-11-22 15:35:59 +0530

    Initial Commit

---
+++ src/main/java/com/mkpits/cricket/CricketManagementJavaApr2023Application.java
@@ -0,0 +1,13 @@
+package com.mkpits.cricket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketManagementJavaApr2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketManagementJavaApr2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkpits/cricket/CricketManagementJavaApr2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkpits.cricket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketManagementJavaApr2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

6166e884d78134c9909ff5d9fd1862f1279a634a
Shreyash199811~ <you@example.com>
2023-11-29 17:31:10 +0530

    first-commit

--- src/main/java/com/mkpits/cricket/CricketManagementJavaApr2023Application.java
+++ src/main/java/com/mkpits/cricket/CricketManagementJavaApr2023Application.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CricketManagementJavaApr2023Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+
+	{
 		SpringApplication.run(CricketManagementJavaApr2023Application.class, args);
 	}
 

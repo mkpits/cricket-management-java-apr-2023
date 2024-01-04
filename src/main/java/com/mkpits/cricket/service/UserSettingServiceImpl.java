149ec3fba0ebe2d4db103e49ef20df7c02fb447a
Pranaylute19 <pranaylute1@gmail.com>
2024-01-04 11:14:31 +0530

    update user setting file

--- src/main/java/com/mkpits/cricket/service/UserSettingServiceImpl.java
+++ src/main/java/com/mkpits/cricket/service/UserSettingServiceImpl.java
@@ -1,8 +1,17 @@
 package com.mkpits.cricket.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserSettingServiceImpl implements UserSettingService{
 
-    private 
+    private UserSettingService userSettingService;
+    @Autowired
+    public UserSettingServiceImpl(UserSettingService userSettingService) {
+        this.userSettingService = userSettingService;
+    }
+
     @Override
     public void updateprofile(int usersettingid) {
 

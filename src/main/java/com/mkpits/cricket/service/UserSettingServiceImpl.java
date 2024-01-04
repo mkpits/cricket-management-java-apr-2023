51551a598c04448827315b0fa46de7974a9d14c9
Pranaylute19 <pranaylute1@gmail.com>
2024-01-04 11:12:27 +0530

    update usersetting files

---
+++ src/main/java/com/mkpits/cricket/service/UserSettingServiceImpl.java
@@ -0,0 +1,10 @@
+package com.mkpits.cricket.service;
+
+public class UserSettingServiceImpl implements UserSettingService{
+
+    private 
+    @Override
+    public void updateprofile(int usersettingid) {
+
+    }
+}

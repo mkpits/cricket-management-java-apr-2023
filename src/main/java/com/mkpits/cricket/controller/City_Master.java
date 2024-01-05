9b9be1ad2bf96a08f8704d48c88dd575d9acde90
Shubham_bhoyar <bhoyarshubham869@gmail.com>
2024-01-05 15:30:12 +0530

    updated code

---
+++ src/main/java/com/mkpits/cricket/controller/City_Master.java
@@ -0,0 +1,10 @@
+//package com.mkpits.cricket.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping("/cities")
+//public class City_Master {
+//
+//}
---
+++ src/main/java/com/mkpits/cricket/service/CityServiceImplementation.java
@@ -0,0 +1,20 @@
+//package com.mkpits.cricket.service;
+//
+//import com.mkpits.cricket.entity.City;
+//import com.mkpits.cricket.repository.CityRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class CityServiceImplementation implements CityService{
+//    private CityRepository cityRepository;
+//    @Autowired
+//    public CityServiceImplementation(CityRepository cityRepository) {
+//        this.cityRepository = cityRepository;
+//    }
+//
+//    @Override
+//    public City insertCity(City city) {
+//        return cityRepository.save(city);
+//    }
+//}

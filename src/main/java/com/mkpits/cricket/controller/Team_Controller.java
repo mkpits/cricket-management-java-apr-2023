ffd937923a76595aaeea3df22a8b0eb0c1caebe4
wakodikarShital <shitalwakodikar436@gmail.com>
2024-01-09 12:50:51 +0530

    Player management controller

--- src/main/java/com/mkpits/cricket/controller/Team_Controller.java
+++ src/main/java/com/mkpits/cricket/controller/Team_Controller.java
@@ -27,51 +27,6 @@ public class Team_Controller {
         this.teamService=teamService;
     }
 
-/** --------------------------- Admin Login form --------------------------- */
-
-    @GetMapping("/adminLoginForm")
-    public String admin_Login(){
-        return "adminLoginForm";
-    }
-
-/** --------------------------- Menu page call when we that end point of "/Dashboard" --------------------------- */
-
-    @GetMapping("/Dashboard")
-    public  String menu_Page(){
-        return "menuPage";
-    }
-
-/** ------------- find list of players ----------------- */
-
-    @GetMapping("/listOfPlayers")
-    public String findListOfPlayers(Model model){
-        List<Players> playersList=playersService.findAllPlayers();
-        model.addAttribute("listOfPlayers",playersList);
-        return "displayPlayersList";
-    }
-
-/** ---------------------------- save method for register players ------------------------- **/
-
-    @GetMapping("/Players")
-    public String playersInfo(Model model){
-        Players players = new Players();
-        model.addAttribute("players" , players);
-        return "registerPlayersInformation";
-    }
-
-    @PostMapping("/Save")
-    public String save(@ModelAttribute("players") Players newPlayers ){
-        playersService.save(newPlayers);
-        return "redirect:/listOfPlayers";
-    }
-
-/** -------------------------- update player -------------------------------------------- **/
-//    @GetMapping("/updatePlayer")
-//    public String updatePlayer(@RequestParam("player_id") int playerId, Model model) {
-//        Players players = playersService.updatePlayers(playerId);
-//        model.addAttribute("players", players);
-//        return "registerPlayersInformation";
-//    }
 
 /** ------------- find list of Teams ----------------- */
 
